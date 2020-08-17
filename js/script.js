9979236ada0db2abd731539872e8fd695359cd6e
Riskal Abdullah Balfas <riskalab@gmail.com>
2020-08-17 19:04:40 +0700

    change protocol to https

--- js/script.js
+++ js/script.js
@@ -643,7 +643,7 @@ function checkingWords(pieces, words, score) {
         promisesOfWords = [],
         mainWord = [...words][0],
         fetchMainWord = () => {
-            fetch(`http://kateglo.com/api.php?format=json&phrase=${mainWord}`)
+            fetch(`https://kateglo.com/api.php?format=json&phrase=${mainWord}`)
                 .then(response => response.json())
                 .finally(() => loading.style.display = ``)
                 .then(response => {
@@ -689,7 +689,7 @@ function checkingWords(pieces, words, score) {
         };
     loading.style.display = `block`;
     words.map(word => {
-        promisesOfWords.push(fetch(`http://kateglo.com/api.php?format=json&phrase=${word}`).then(response => response.json()))
+        promisesOfWords.push(fetch(`https://kateglo.com/api.php?format=json&phrase=${word}`).then(response => response.json()))
     });
     Promise.all(promisesOfWords)
         .finally(() => loading.style.display = ``)
